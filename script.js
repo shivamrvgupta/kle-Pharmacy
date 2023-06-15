510726d5fbf8247d14f4bb3cf57cbc2417d23569
Shivam Gupta <shivamgupta@Shivams-MacBook-Air.local>
2023-06-15 16:48:20 +0530

    added appscript

---
+++ script.js
@@ -0,0 +1,23 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzW80MAEt-A1vVGxN817A6FhZmsWieI1SqK0zV9rYJ-3IpvAe8j3-6tVNmYiEXa6M731A/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        setTimeout(function(){
+            window.location.href = './Thankyou.html';
+            msg.innerHTML = "Message sent successfully"
+        },2000)
+        form.reset()
+    })
+    .catch(error => {
+        msg.innerHTML = "There is Error, Please Check the values again"
+        setTimeout(function(){
+        msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+})
+                  
